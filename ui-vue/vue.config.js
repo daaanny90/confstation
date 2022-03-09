9bad2f6bd893a5f346d8fc339662915fca289abf
Danny Spina <danny@ostec.de>
2022-03-09 11:41:25 +0100

    Fix stylesheet import order

--- ui-vue/vue.config.js
+++ ui-vue/vue.config.js
@@ -3,9 +3,10 @@ module.exports = {
     loaderOptions: {
       sass: {
         prependData: `
-        @import "@/globals/scss/_bootstrap-override.scss";
-        @import "./node_modules/bootstrap/scss/bootstrap.scss";
         @import "@/globals/scss/_variables.scss";
+        @import "@/globals/scss/_bootstrap-override.scss";
+        @import "@/globals/scss/_global.scss";
+        @import "./node_modules/bootstrap/scss/bootstrap.scss";
         `
       }
     }
